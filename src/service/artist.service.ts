f61ed1cd409122a7f3dcf6629da22e26667b3550
Cory Hack <cory.hack@gmail.com>
2019-12-10 22:06:23 -0500

    Fix auth login/signup, add token to delete artist

--- src/service/artist.service.ts
+++ src/service/artist.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import {Artist} from '../models/artist';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
+import { Router } from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +12,13 @@ export class ArtistService {
 //HEROKU URL
 private artistUrl = 'http://dr-server.herokuapp.com/artist';
 
-// LOCAL URL
-// private artistsUrl = 'http://localhost:3000/artist/
-// private artistUrl = 'http://dr-server.herokuapp.com/artist'
-
 private token: string;
 
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private router: Router
+    ) { }
 
   getToken() {
     return this.token;
@@ -27,6 +27,13 @@ private token: string;
   createArtist(firstName: string, lastName: string, password: string, email: string, role: string){
     const artist: Artist = { firstName: firstName, lastName: lastName,  password: password, email: email, role: role}
     return this.http.post<any>(this.artistUrl+ '/register', artist)
+    .subscribe(response => {
+      const token = response.sessionToken;
+      this.token = token;
+      console.log(token)
+      this.saveAuthData(token)
+      this.router.navigate(["artists"])
+    })
   }
 
   loginArtist(password: string, email: string){
@@ -37,6 +44,7 @@ private token: string;
       this.token = token;
       console.log(token)
       this.saveAuthData(token)
+      this.router.navigate(["artists"])
     })
   }
 
@@ -52,21 +60,23 @@ private token: string;
   }
 
   deleteArtist (artistId){
-    console.log(this.artistUrl)
-    return this.http.delete<Artist[]>(this.artistUrl + '/delete' + `/${artistId}`).subscribe(()=> {
-      console.log("Deleted")
-      this.getArtists();
-      
+    const token = localStorage.getItem('token');
+     return this.http.delete<Artist[]>(this.artistUrl + '/delete' + `/${artistId}`, { headers: {'Authorization': token}})
+    .subscribe(()=> {
+      // this.getArtists();
+      this.router.navigate(["admin"])
     })
   }
 
 
-  // updateArtist (artistId){
-  //   console.log(this.artistUrl)
-  //   return this.http.put<Artist[]>(this.artistUrl + '/delete' + `/${artistId}`).subscribe(()=> {
-  //     console.log("Updated")
-  //   })
-  // }
+  updateArtist (artistId, firstName: string, lastName: string, password: string, email: string, role: string){
+    const artist = { firstName: firstName, lastName: lastName,  password: password, email: email, role: role}
+   
+    return this.http.put(this.artistUrl + '/update' + `/${artistId}`, artist, { headers: {'Authorization': localStorage.getItem('token')}})
+    .subscribe(()=> {
+      this.router.navigate(["/"])
+    })
+  }
 
 
   private saveAuthData(token: string){
