56163d2ca80ec901f78230dcf479f15c9f020739
Devon Mason <devonmmason@gmail.com>
2019-12-10 23:32:51 -0500

    toggle between login and register

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,40 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { AuthComponent } from '../auth/auth.component';
 import { LoginComponent } from '../login/login.component';
-import { AuthService } from './../auth/auth.service';
+import { ArtistService } from '../../service/artist.service'
+import { Subscription } from 'rxjs'
 
-
-// export interface DialogData {
-//   email: string;
-//   password: string;
-// }
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements OnInit {
-  // email: string;
-  // password: string;
-
-  constructor(public auth: AuthService) { }
-
-  // openDialog(): void {
-  //   const dialogRef = this.dialog.open(LoginComponent, {
-      
-  //   });
-
-  //   dialogRef.afterClosed().subscribe(result => {
-  //     // console.log('The dialog was closed');
-  //     // this.email = result;
-  //   });
-  // }
+export class HomePageComponent implements OnInit, OnDestroy {
+  artistIsAuthenticated = false;
+  private authListenerSubs: Subscription
   
+  constructor(private artistService: ArtistService) { }
+
+ 
 
   ngOnInit() {
+    this.authListenerSubs = this.artistService.getAuthStatusListener()
+    .subscribe(
+      isAuthenticated =>{
+          this.artistIsAuthenticated = isAuthenticated;
+      }
+    );
     
+  }
+
+  ngOnDestroy() {
+    this.authListenerSubs.unsubscribe();
+
   }
 
 }
