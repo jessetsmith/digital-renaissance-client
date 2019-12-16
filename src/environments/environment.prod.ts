6e1e2ddf55b193dbd137a9dadd898e846d634356
Devon Mason <devonmmason@gmail.com>
2019-12-16 13:22:22 -0500

    first commit for client

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+
+  export let APIURL = '';
+  
+  switch (window.location.hostname) {
+    // this is the deployed angular application
+    case 'kcp-pieclient.herokuapp.com':
+        // this is the full url of your deployed API
+        APIURL = 'https://kcp-pieapi.herokuapp.com'
+        break;
+    default:
+        // this is the local host name of your API
+        APIURL = 'http://localhost:<Port of your local API>';
+}
+
+
 };
